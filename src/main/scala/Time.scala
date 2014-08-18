7ddf71a03755900f95cd5c1769c2a0092b6b2d54
Irina Dumler <onoprodum@googlemail.com>
2014-08-18 23:55:18 +0200

    comply to tests

--- src/main/scala/Time.scala
+++ src/main/scala/Time.scala
@@ -5,17 +5,20 @@ package org.scalatrain
  */
 object Time {
   def fromMinutes( minutes: Int ) = {
-    // TODO check preconditions
+    require( minutes >= 0, "minutes must not be negative")
     new Time( minutes / 60, minutes % 60 )
   }
 }
 
 case class Time( hours: Int = 0, minutes: Int = 0 ) {
-
-  // TODO check preconditions
+  require( hours >= 0, "hours must not be negative")
+  require( hours < 24, "hours must be >= 24")
+  require( minutes >= 0, "minutes must not be negative")
+  require( minutes < 60, "minutes must not >= 60")
 
   lazy val asMinutes: Int = hours* 60 + minutes
   def -( that: Time ) : Int = {
+    require( that != null, "that must not be null")
     asMinutes - that.asMinutes
   }
 }
